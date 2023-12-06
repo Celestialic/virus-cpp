093e939086d58251476ac8feff248e3127f34151
Shevtsov German <schevtsov.german@yandex.ru>
2023-12-06 21:52:53 +0300

    v6

--- g++.bat
+++ g++.bat
@@ -1,6 +1,6 @@
 cd cpp
 g++ -o memory_spam.exe memory_spam.cpp
-g++ -o monitor_blink.exe monitor_blink.cpp
+g++ -o error_spam.exe error_spam.cpp
 g++ -o window_spam.exe window_spam.cpp
 g++ -o taskmgr_killer.exe taskmgr_killer.cpp
 g++ -o mouse_locker.exe mouse_locker.cpp
